2872a05f8eaeebddf30ea53abcf511f8092d71e3
RuslanMiki <mikitchenko_ruslan@ukr.net>
2022-11-20 15:54:13 +0100

    1 Task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+const gallery = document.querySelector('.gallery');
+const render = galleryItems
+  .map(
+    item => `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}"/>
+  </a>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', render);
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
 
 console.log(galleryItems);
